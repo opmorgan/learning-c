72ddee2d990924d5e43a8e34f4b756d8390de234
Owen Morgan <owen.morgan95@gmail.com>
2020-07-07 19:32:09 -0400

    to section 1.5

--- chapter-1/for.c
+++ chapter-1/for.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
+
 /* print Fahr-Celsius Table */
 main()
 {
   float fahr;
   float celsius;
 
-  for (fahr = 300; fahr >= 0; fahr = fahr - 20) {
+  for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP) {
     celsius = ((5.0/9.0)*(fahr-32));
     printf("%3.0f %6.1f\n", fahr, celsius);
   }
