51528fb6d50c6adbcf523a47f3e0e5657d35739b
Owen Morgan <owen.morgan95@gmail.com>
2020-07-12 03:14:24 -0400

    cleans up 1-14

--- chapter-1/14_charhist.c
+++ chapter-1/14_charhist.c
@@ -2,12 +2,15 @@
 
 #include <stdio.h>
 
+#define ARRAY_SIZE(x) (sizeof((x)) / sizeof((x)[0]))
+
 /* the maximum number of different chars the program will be able to count */
 #define MAX_CHARS 255 
 /* the maximum number of pips to display in the histogram */
 #define MAX_PIPS 50
 
-/* todo: try making a function to generate the length needed for an array to hold the characters defined in a macro */
+int find_max(int * array);
+int find_ndigits(int num);
 
 int main(void) {
   /* create array with characters to count */
@@ -19,65 +22,44 @@ int main(void) {
   int n_chars = (sizeof(chars) / sizeof(chars[0]));
 
   /* create array to hold counts of each character */
-  int freqs[MAX_CHARS] = {0};
+  int counts[MAX_CHARS] = {0};
 
+  /* populate array with counts of each character */
+  /* counts = get_charcounts(chars) */
   int c;
   while((c = getchar()) != EOF) {
     for (int i = 0; i <= n_chars; ++i) {
       /* count occurences (-20 for uppercase) */
       if ( (c == chars[i]) | (c == chars[i] - 20) )
-        ++freqs[i];
+        ++counts[i];
     }
   }
 
-  /* display histogram data */
+  /* prepare data for histogram */
 
   /* find the greatest count in the array */
-  /* int greatest_count = find_max(freqs); */
-  /* int find_max(int array) { */
-  /*   int greatest_count = 0; */
-  /*   for (int i = 0; i <= n_chars-1; ++i) { */
-  /*     if (freqs[i] > greatest_count) { */
-  /*       greatest_count = freqs[i]; */
-  /*     } */
-  /*   } */
-  /* } */
-  int greatest_count = 0;
-  for (int i = 0; i <= n_chars-1; ++i) {
-    if (freqs[i] > greatest_count) {
-      greatest_count = freqs[i];
-    }
-  }
+  int greatest_count = find_max(counts);
 
   /* find the length of its decimal representation */
-  int n = greatest_count; int greatest_len = 0;
-  while (n != 0) {
-    n = n/10;
-    ++greatest_len;
-  }
-  printf("Greatest count: %d. Length: %d.\n", greatest_count, greatest_len);
-
+  int greatest_len = find_ndigits(greatest_count);
 
   /* scale histogram, if needed */
-  /* freqs_scaled = scale_histdata(freqs, int MAXPIPS); */
   int max_pips = MAX_PIPS;
   int scale_factor = greatest_count / max_pips;
-  printf("Scale factor: %d ( X %d = %d\n", scale_factor, max_pips, greatest_count);
-  int freqs_scaled[MAX_CHARS] = {0};
+  int counts_scaled[MAX_CHARS] = {0};
   for (int i = 0; i <= n_chars-1; ++i) {
-    printf("in for loop. i = %d; letter = %c; greatest_count = %d; max_pips = %d; (greatest_count > max_pips) = %d; freqs[i] = %d; ", i, chars[i], greatest_count, max_pips, (greatest_count > max_pips), freqs[i]); 
     if (greatest_count > max_pips) {
-      freqs_scaled[i] = freqs[i] / scale_factor;
+      counts_scaled[i] = counts[i] / scale_factor;
     } else {
-      freqs_scaled[i] = freqs[i];
+      counts_scaled[i] = counts[i];
     }
-    printf("freqs_scaled[i] = %d.\n", freqs_scaled[i]);
   }
 
+  /* display histogram  */
   for (int i = 0; i <= n_chars-1; ++i) {
-    printf("%c: %*d |", chars[i], 5, freqs[i]);
-    for (int j = 0; j <= freqs_scaled[i] - 1; ++j) {
-      if (j < freqs_scaled[i] - 1) {
+    printf("%c: %*d |", chars[i], 5, counts[i]);
+    for (int j = 0; j <= counts_scaled[i] - 1; ++j) {
+      if (j < counts_scaled[i] - 1) {
         printf("%s", "-");
       } else 
         printf("%c", chars[i]);
@@ -85,3 +67,24 @@ int main(void) {
     printf("\n");
   }
 }
+
+int find_max(int * array) {
+  int array_len = ARRAY_SIZE(array);
+  int max = 0;
+  for (int i = 0; i <= array_len-1; ++i) {
+    if (array[i] > max) {
+      max = array[i];
+    }
+  }
+  return max;
+}
+
+int find_ndigits(int num) {
+  int ndigits= 0; int n = num;
+  while (n != 0) {
+    n = n/10;
+    ++ndigits;
+  }
+  return n;
+}
+
