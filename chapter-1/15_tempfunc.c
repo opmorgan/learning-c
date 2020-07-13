331de1071e5403d552361c9f83c0f2bba9240bce
Owen Morgan <owen.morgan95@gmail.com>
2020-07-12 23:10:24 -0400

    adds exercise 1-15

---
+++ chapter-1/15_tempfunc.c
@@ -0,0 +1,33 @@
+/* Print a Fahrenheit-Celsius conversion table. */
+
+#include <stdio.h>
+
+#define LOWER 0.0
+#define UPPER 300.0
+#define STEP 20.0
+
+float fahr2cel(int fahr);
+
+int main(void)
+{
+  float fahr, celsius;
+  fahr = LOWER;
+
+  /* print header */
+  printf("%s\t%s\n", "fahrenheit", "celsius");
+
+  /* print fahrenheit-celsius conversion table */
+  while (fahr <= UPPER) {
+    celsius = fahr2cel(fahr); 
+    printf("%10.0f\t%7.1f\n", fahr, celsius);
+    fahr = fahr + STEP;
+  }
+  printf("\n");
+
+  return 0;
+}
+
+float fahr2cel(int fahr) {
+  float celsius = (5.0/9.0) * (fahr-32.0);
+  return celsius;
+}
